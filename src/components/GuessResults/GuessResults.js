d9defa1c69ad344febca08fdd241169dee68a802
ali-hbl <nwah.ally@gmail.com>
2024-07-07 17:18:50 +0200

    table for guesses

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { range } from '../../utils';
+import Guess from '../Guess/Guess';
 
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {guesses.map((guess, i) => (
-        <p className="guess" key={i}>
-          {guess}
-        </p>
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} value={guesses[num]} />
       ))}
     </div>
   );
