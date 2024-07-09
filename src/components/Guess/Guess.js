a4e5f9a4724da9490e7c85e90edbddbcee4619bc
ali-hbl <nwah.ally@gmail.com>
2024-07-09 15:40:48 +0200

    game logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { checkGuess } from '../../game-helpers';
 import { range } from '../../utils';
 
-function Guess({ value }) {
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell';
+
+  return <span className={className}>{letter}</span>;
+}
+
+function Guess({ value, answer }) {
+  const result = checkGuess(value, answer);
+
   return (
     <div className="grid">
       <p className="guess">
-        {range(5).map((num) => (
-          <span key={num} className="cell">
-            {value ? value[num] : undefined}
-          </span>
+        {range(5).map((i) => (
+          <Cell
+            key={i}
+            letter={result ? result[i].letter : undefined}
+            status={result ? result[i].status : undefined}
+          />
         ))}
       </p>
     </div>
